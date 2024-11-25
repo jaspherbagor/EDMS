2908109fff9dcbd0df9a7ca9ec0d684172335401
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-11-25 20:41:06 +0800

    made the login page functional

--- EDMS/resources/views/admin/layout/app.blade.php
+++ EDMS/resources/views/admin/layout/app.blade.php
@@ -34,8 +34,5 @@
     </div>
 </div>
 
-<script src="dist/js/scripts.js"></script>
-<script src="dist/js/custom.js"></script>
-
 </body>
 </html>
--- EDMS/resources/views/admin/layout/styles.blade.php
+++ EDMS/resources/views/admin/layout/styles.blade.php
@@ -1,16 +1,16 @@
-    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="dist/css/font_awesome_5_free.min.css">
-    <link rel="stylesheet" href="dist/css/select2.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-timepicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap-tagsinput.css">
-    <link rel="stylesheet" href="dist/css/duotone-dark.css">
-    <link rel="stylesheet" href="dist/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="dist/css/iziToast.min.css">
-    <link rel="stylesheet" href="dist/css/fontawesome-iconpicker.min.css">
-    <link rel="stylesheet" href="dist/css/bootstrap4-toggle.min.css">
-    <link rel="stylesheet" href="dist/css/summernote-bs4.css">
-    <link rel="stylesheet" href="dist/css/style.css">
-    <link rel="stylesheet" href="dist/css/components.css">
-    <link rel="stylesheet" href="dist/css/spacing.css">
-    <link rel="stylesheet" href="dist/css/custom.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font_awesome_5_free.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-datepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-timepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-tagsinput.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/duotone-dark.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/iziToast.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/fontawesome-iconpicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap4-toggle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/summernote-bs4.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/spacing.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
