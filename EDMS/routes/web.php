754ca8c6f70c7ea4b78e778a705c20784a163173
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-11-25 20:17:35 +0800

    added assets and set up a page for the login

--- EDMS/routes/web.php
+++ EDMS/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthenticationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AdminAuthenticationController::class, 'index'])->name('login');
