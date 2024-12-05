671c4fd4951083ae2a2114a1d4bdc56e3612bd17
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-12-05 20:25:43 +0800

    resolved the conflict

--- EDMS/resources/views/admin/employee_list.blade.php
+++ EDMS/resources/views/admin/employee_list.blade.php
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-<h1>This is a list of all the employees in the corporation...</h1>
-=======
 <h1>This is an employee list</h1>
->>>>>>> 2880565398549176f3f5b71c1d39f81727531853
-u9y9youyi
+
