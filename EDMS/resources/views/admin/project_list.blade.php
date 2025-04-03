42b6223e658ce1046839a9ba938ac7a8eb518446
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-04-03 19:19:57 +0800

    added a page for the project list

---
+++ EDMS/resources/views/admin/project_list.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layout.app')
+
+@section('heading', 'All Projects')
+
+@section('right_top_button')
+<a href="" class="btn btn-primary"><i class="fa fa-plus"></i> Add New</a>
+@endsection
+@section('main_content')
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="example1">
+                                <thead>
+                                    <tr>
+                                        <th>SL</th>
+                                        <th>Name</th>
+                                        <th>Photo</th>
+                                        <th>Location</th>
+                                        <th>Client Name</th>
+                                        <th>Admin</th>
+                                        <th>Current Progress</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Little Moses Learning Center</td>
+                                        <td><img src="" alt="slide_image" class="w_200"></td>
+                                        <td>Vista Mall - General Trias</td>
+                                        <td>Jane Doe</td>
+                                        <td>Baby Javier</td>
+                                        <td>50%</td>
+                                        <td class="pt_10 pb_10">
+                                            <a href="" class="btn btn-primary">Edit</a>
+                                            <a href="" class="btn btn-danger" onClick="return confirm('Are you sure you want to delete?');">Delete</a>
+                                        </td>
+                                        
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
